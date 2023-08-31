55be355de334e34d877377f938a8712532fb1a93
JRSiwiecki <JRSiwiecki@gmail.com>
2023-08-30 19:59:56 -0700

    Add feature-based folder structure + test files + update example test file

--- tests/examples/example.spec.ts
+++ tests/examples/example.spec.ts
@@ -1,18 +1,23 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// Use descriptive test titles, or what is the test literally testing?
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
+  // Make sure tests have some expectation that ensures a certain feature is working as intended,
+  // or what is the assertion/expectation?
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole("link", { name: "Get started" }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
 });
