222547a12745ac6b9cbb96d7d2e57e57c46036e0
JRSiwiecki <JRSiwiecki@gmail.com>
2024-04-06 16:02:16 -0700

    Remove unused import

--- tests/user-profiles/view-personal-profile.spec.ts
+++ tests/user-profiles/view-personal-profile.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, Locator } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test.describe("view personal profile tests", () => {
     test.beforeEach(async ({ page }) => {
